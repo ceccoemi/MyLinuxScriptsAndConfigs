788b2d4aa079b1d9515c6608db4d91573cea087a
Emilio Cecchini <cecchinator@gmail.com>
2018-04-19 20:25:41 +0200

    Move installer outside scripts and bug fixes

--- install_scripts.sh
+++ install_scripts.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+# it holds /home/<user>, even if this script is executed with sudo
 REAL_HOME="/home/$(who | awk '{print $1}')"
 
 function copyScript {
-	cp $(dirname $0)/$file $REAL_HOME/bin
-	chmod 744 $REAL_HOME/bin/$1
-	chgrp $(who | awk '{print $1}') "$REAL_HOME/bin/$1"
-	chown $(who | awk '{print $1}') "$REAL_HOME/bin/$1"
+	echo "Copying $1"
+	cp "$1" "$REAL_HOME/bin"
+	chmod 744 "$REAL_HOME/bin/$(basename $1)"
+	chgrp $(who | awk '{print $1}') "$REAL_HOME/bin/$(basename $1)"
+	chown $(who | awk '{print $1}') "$REAL_HOME/bin/$(basename $1)"
 }
 
 if [ $(id -u) != 0 ]; then
@@ -21,8 +23,8 @@ if [ ! -d "$REAL_HOME/bin" ] ; then
 	chown $(who | awk '{print $1}') "$REAL_HOME/bin"
 	if [ ! $(grep 'PATH="\$REAL_HOME/bin:\$PATH"' $REAL_HOME/.profile) ] ; then
 		# Add $REAL_HOME/bin folder to the executable path
-		echo "" >> $REAL_HOME/.profile
-		echo "# set PATH so it includes user's private bin if it exists" >> $REAL_HOME/.profile
+		echo "" >> "$REAL_HOME/.profile"
+		echo "# set PATH so it includes user's private bin" >> $REAL_HOME/.profile
 		echo 'if [ -d "$REAL_HOME/bin" ] ; then' >> "$REAL_HOME/.profile"
 		echo '    PATH="$REAL_HOME/bin:$PATH"' >> "$REAL_HOME/.profile"
 		echo 'fi' >> "$REAL_HOME/.profile"
@@ -38,21 +40,20 @@ fi
 echo "Copying scripts to $REAL_HOME/bin"
 # Here add the scripts that must be executed with sudo
 sudoScripts="up"
-if [ $(id -u) = 0 ]; then
-	# If the script is executed with sudo
-	for file in $(ls $(dirname "$0")); do
-		if [ $file != $(basename "$0") ]; then
-			copyScript $file
-			if [ "$(echo $sudoScripts | grep $file)" ]; then
-				# For sudo scripts make a link to /usr/local/sbin, so they are callable with sudo
-				ln -svi "$REAL_HOME/bin/$file" "/usr/local/sbin/$file"
-			fi
+if [ $(id -u) = 0 ]; then  # If the script is executed with sudo
+	echo ---
+	echo $(ls $(dirname "$0")/scripts)
+	echo ---
+	for file in $(ls $(dirname "$0")/scripts); do
+		copyScript "$(dirname "$0")/scripts/$file"
+		if [ "$(echo $sudoScripts | grep $file)" ]; then
+			# Make a link to /usr/local/sbin, so they are callable with sudo
+			ln -svi "$REAL_HOME/bin/$file" "/usr/local/sbin/$file"
 		fi
 	done
-else
-	# If the script is not executed with sudo
-	for file in $(ls $(dirname "$0")); do
-		if [ "$file" != $(basename "$0") ] && [ ! "$(echo $sudoScripts | grep $file)" ]; then
+else  # If the script is not executed with sudo
+	for file in $(ls $(dirname "$0")/scripts); do
+		if [ ! "$(echo $sudoScripts | grep $file)" ]; then
 			copyScript "$file"
 		fi
 	done
