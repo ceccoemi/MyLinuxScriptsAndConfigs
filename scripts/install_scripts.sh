612ee723b15e2ed6e0e5bad3b3c304ff901376bb
Emilio Cecchini <cecchinator@gmail.com>
2018-04-19 19:36:24 +0200

    Fix install_scripts.sh, now it always installs bin folder in /home/user

--- scripts/install_scripts.sh
+++ scripts/install_scripts.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+REAL_HOME="/home/$(who | awk '{print $1}')"
+
 function copyScript {
-	cp $(dirname $0)/$file $HOME/bin
-	chmod 744 $HOME/bin/$1
-	chgrp $(who | awk '{print $1}') "$HOME/bin/$1"
-	chown $(who | awk '{print $1}') "$HOME/bin/$1"
+	cp $(dirname $0)/$file $REAL_HOME/bin
+	chmod 744 $REAL_HOME/bin/$1
+	chgrp $(who | awk '{print $1}') "$REAL_HOME/bin/$1"
+	chown $(who | awk '{print $1}') "$REAL_HOME/bin/$1"
 }
 
 if [ $(id -u) != 0 ]; then
@@ -12,28 +14,28 @@ if [ $(id -u) != 0 ]; then
 will not install those scripts that need superuser privileges"
 fi
 
-if [ ! -d "$HOME/bin" ] ; then
-	echo "Setting up $HOME/bin"
-	mkdir "$HOME/bin" -m 755
-	chgrp $(who | awk '{print $1}') "$HOME/bin"
-	chown $(who | awk '{print $1}') "$HOME/bin"
-	if [ ! $(grep 'PATH="\$HOME/bin:\$PATH"' $HOME/.profile) ] ; then
-		# Add $HOME/bin folder to the executable path
-		echo "" >> $HOME/.profile
-		echo "# set PATH so it includes user's private bin if it exists" >> $HOME/.profile
-		echo 'if [ -d "$HOME/bin" ] ; then' >> "$HOME/.profile"
-		echo '    PATH="$HOME/bin:$PATH"' >> "$HOME/.profile"
-		echo 'fi' >> "$HOME/.profile"
+if [ ! -d "$REAL_HOME/bin" ] ; then
+	echo "Setting up $REAL_HOME/bin"
+	mkdir "$REAL_HOME/bin" -m 755
+	chgrp $(who | awk '{print $1}') "$REAL_HOME/bin"
+	chown $(who | awk '{print $1}') "$REAL_HOME/bin"
+	if [ ! $(grep 'PATH="\$REAL_HOME/bin:\$PATH"' $REAL_HOME/.profile) ] ; then
+		# Add $REAL_HOME/bin folder to the executable path
+		echo "" >> $REAL_HOME/.profile
+		echo "# set PATH so it includes user's private bin if it exists" >> $REAL_HOME/.profile
+		echo 'if [ -d "$REAL_HOME/bin" ] ; then' >> "$REAL_HOME/.profile"
+		echo '    PATH="$REAL_HOME/bin:$PATH"' >> "$REAL_HOME/.profile"
+		echo 'fi' >> "$REAL_HOME/.profile"
 
-		source .profile
+		source "$REAL_HOME/.profile"
 
-		echo "Added $HOME/bin folder to PATH"
+		echo "Added $REAL_HOME/bin folder to PATH"
 	fi
 else
-	echo "$HOME/bin directory already found, assuming that is already in PATH"
+	echo "$REAL_HOME/bin directory already found, assuming that is already in PATH"
 fi
 
-echo "Copying scripts to $HOME/bin"
+echo "Copying scripts to $REAL_HOME/bin"
 # Here add the scripts that must be executed with sudo
 sudoScripts="up"
 if [ $(id -u) = 0 ]; then
@@ -43,7 +45,7 @@ if [ $(id -u) = 0 ]; then
 			copyScript $file
 			if [ "$(echo $sudoScripts | grep $file)" ]; then
 				# For sudo scripts make a link to /usr/local/sbin, so they are callable with sudo
-				ln -svi "$HOME/bin/$file" "/usr/local/sbin/$file"
+				ln -svi "$REAL_HOME/bin/$file" "/usr/local/sbin/$file"
 			fi
 		fi
 	done
